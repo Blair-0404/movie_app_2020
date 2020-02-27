7fbb9220538e99ff96356438d3d7ad5c641ed569
Rachel4858 <rachel458458@gmail.com>
2020-02-27 16:54:30 +0900

    Updates

---
+++ precache-manifest.c58317195f6d103ff6f413b1f0a6aad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96db55ddaee0af624cadab724b71beac",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2a4120f480df877209de",
+    "url": "/movie_app_2020/static/css/main.eabbc2b9.chunk.css"
+  },
+  {
+    "revision": "e3aca23ad6fcf7f07508",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4120f480df877209de",
+    "url": "/movie_app_2020/static/js/main.b3dd39ee.chunk.js"
+  },
+  {
+    "revision": "21664dc3e2d58fbc7515",
+    "url": "/movie_app_2020/static/js/runtime-main.de86b078.js"
+  }
+]);
