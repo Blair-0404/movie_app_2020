9187f3fb0541c9f36621e0a41e1cb35683554555
Rachel4858 <rachel458458@gmail.com>
2020-02-28 23:17:04 +0900

    Adding Route

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css"
-import "./reset.css"
+import "../reset.css"
 
 function Movie({id, year, title, summary, poster, genres}) {
   return (
+    <Link to={{
+      pathname:`/movie/${id}`,
+      state: {
+        year,
+        title,
+        summary,
+        poster,
+        genres
+      }
+    }}>
     <div className="movie">
       <div className="movie-img-wrapper">
         <img src={poster} className="movie-img" alt={title} title={title}/>
@@ -19,6 +30,7 @@ function Movie({id, year, title, summary, poster, genres}) {
         <div className="movie__summary">{summary.slice(0, 180)}...</div>
       </div>
     </div>
+    </Link>
   );
 }
 
